f5b704f90f93aded9643aefe75203888719ec6c0
Santiago <lpzsd@gmail.com>
2021-12-05 20:47:17 -0500

    fixing settings_prod link in manage.py file

--- deployment.sh
+++ deployment.sh
@@ -1,4 +1,4 @@
 sudo heroku login
 sudo heroku container:login
-sudo heroku container:push web --app misiontic-2021-auth-ms
-sudo heroku container:release web --app misiontic-2021-auth-ms
+sudo heroku container:push web --app factura-plaza-ms
+sudo heroku container:release web --app factura-plaza-ms
